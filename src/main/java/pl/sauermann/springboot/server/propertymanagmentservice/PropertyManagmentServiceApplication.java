35ae02e60c85ff00b361ab9511e97896009c23de
Sauermann <sauermad@emea.roche.com>
2019-06-19 11:59:12 +0200

    Project initialization

---
+++ src/main/java/pl/sauermann/springboot/server/propertymanagmentservice/PropertyManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.sauermann.springboot.server.propertymanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyManagmentServiceApplication.class, args);
+    }
+
+}
